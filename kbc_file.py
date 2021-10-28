590825eda1f4ea8a615c93b50224dfe761bf65d0
Satyam kushwaha <91447337+satyamkushwaha11@users.noreply.github.com>
2021-10-28 17:24:06 +0530

    main file

--- kbc_file.py
+++ kbc_file.py
@@ -1,5 +1,7 @@
+#In this KBC(kone banega cororpati) game computer ask the question and player have to give answer, right answer help to win money and one wrong answer can
+#loss the game . you have 5 life line you can use any on them only one times.
 import random,time
-import pyttsx3
+import pyttsx3  # this module use for taxt to speech mode
 
 # from pyttsx3 import engine
 
